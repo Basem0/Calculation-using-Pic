e7d8a292acc98660c58b69d0453d9b45080ec509
Ahmed Basem <126205898+Basem0@users.noreply.github.com>
2024-01-30 18:43:23 +0200

    Delete build/default directory

--- build/default/debug/MCAL_Layer/device_config.p1.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/MCAL_Layer/device_config.p1:  \
-MCAL_Layer/device_config.c  \
-MCAL_Layer/device_config.h 
--- build/default/production/Algorithms/Infix_to_postfix/infix_to_post.p1.d
+++
@@ -1,6 +0,0 @@
-build/default/production/Algorithms/Infix_to_postfix/infix_to_post.p1:  \
-Algorithms/Infix_to_postfix/infix_to_post.c  \
-Algorithms/Infix_to_postfix/infix_to_post.h  \
-Algorithms/Infix_to_postfix/../../MCAL_Layer/mcal_std_types.h  \
-Algorithms/Infix_to_postfix/../../MCAL_Layer/std_libraries.h  \
-Algorithms/Infix_to_postfix/../../MCAL_Layer/compiler.h 
